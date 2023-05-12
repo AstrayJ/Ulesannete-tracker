cefb725edcee5eafa0c04bfab0eec7dba720195d
AstrayJ <jarljustus.hellat@real.edu.ee>
2023-05-12 11:01:00 +0300

    J

--- peamine/kalender/views.py
+++ peamine/kalender/views.py
@@ -7,7 +7,7 @@ from django.http.response import HttpResponse
  
  
 def home_page_view(request):
-   return HttpResponse('You are viewing the home page')
+   return render(request,"LandingPage.html")
 
 def login_page_view(request):
    return render(request, "index.html", "style.css")
