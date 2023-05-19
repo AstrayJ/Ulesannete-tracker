736d16654d2faca3f819172524cecdc4c68bddd1
AstrayJ <jarljustus.hellat@real.edu.ee>
2023-05-19 11:16:02 +0300

    J

--- peamine/kalender/models.py
+++ peamine/kalender/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 class Event(models.Model):
     Sündmus= models.CharField(max_length=100)
+    Lisa = models.CharField(max_length=150)
     Olulisus= models.IntegerField(
         validators=[MaxValueValidator(5), MinValueValidator(1)]
     )
-    Kuupäev= models.DateField(_(""), auto_now=False, auto_now_add=False)
+    Algus kuupäev= models.DateField()
+    Lõpp kuupäev= models.DateField()
