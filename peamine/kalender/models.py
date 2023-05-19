f3516a4992076448f303bda477b164cec47186b1
ProgrammerBro1234 <karlerik.lehtma@real.edu.ee>
2023-05-19 11:27:42 +0300

    f

--- peamine/kalender/models.py
+++ peamine/kalender/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    Sündmus= models.CharField(max_length=100)
+    Olulisus= models.IntegerField(
+        validators=[MaxValueValidator(5), MinValueValidator(1)]
+    )
+    Kuupäev= models.DateField(_(""), auto_now=False, auto_now_add=False)
