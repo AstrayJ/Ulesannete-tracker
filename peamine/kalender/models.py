5d376e6dbdececccbd45307732a17ee7da3ab5bc
ProgrammerBro1234 <131437010+ProgrammerBro1234@users.noreply.github.com>
2023-05-19 20:45:43 +0300

    k

--- peamine/kalender/models.py
+++ peamine/kalender/models.py
@@ -1,18 +1,21 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.translation import gettext_lazy as _
+
 
 class Event(models.Model):
-    Sündmus= models.CharField(max_length=100)
+    Sundmus= models.CharField(max_length=100)
 
     Olulisus= models.IntegerField(
         validators=[MaxValueValidator(5), MinValueValidator(1)]
     )
-    Kuupäev= models.DateField(_(""), auto_now=False, auto_now_add=False)
+    Kuupaev= models.DateField(_(""), auto_now=False, auto_now_add=False)
 
     Lisa = models.CharField(max_length=150)
 
     Olulisus= models.IntegerField()
-    Alguskuupäev= models.DateField()
-    Lõppkuupäev= models.DateField()
+    Alguskuupaev= models.DateField()
+    Loppkuupaev= models.DateField()
 
     Olulisus= models.IntegerField(
         validators=[MaxValueValidator(5), MinValueValidator(1)]
