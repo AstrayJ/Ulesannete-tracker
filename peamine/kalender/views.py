f0ef150c64c36d662dc147b25b201d8648e6a108
funny man <frankplays678@gmail.com>
2023-05-19 10:33:26 +0300

    am

--- peamine/kalender/views.py
+++ peamine/kalender/views.py
@@ -19,12 +19,12 @@ from django.contrib import messages
 
 def register_page_view(request):
    if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("main:homepage")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render (request=request, template_name="main/register.html", context={"register_form":form})
+      form = NewUserForm(request.POST)
+      if form.is_valid():
+         user = form.save()
+         login(request, user)
+         messages.success(request, "Registration successful." )
+         return redirect("main:homepage")
+      messages.error(request, "Unsuccessful registration. Invalid information.")
+   form = NewUserForm()
+   return render (request=request, template_name="main/register.html", context={"register_form":form})
