b7cbffbf3085e31bb67557b06be78de77a3da2f5
ProgrammerBro1234 <131437010+ProgrammerBro1234@users.noreply.github.com>
2023-05-19 18:58:06 +0300

    f

--- peamine/kalender/forms.py
+++ peamine/kalender/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-<<<<<<< HEAD
+
 
 
 # Create your forms here.
-=======
+
 from django.forms import ModelForm
 
 
 
->>>>>>> f726452b778ad5e6e15ee4fe5a4249665ad5befb
+
 
 class NewUserForm(UserCreationForm):
 	email = forms.EmailField(required=True)
@@ -24,15 +24,11 @@ class NewUserForm(UserCreationForm):
 		user.email = self.cleaned_data['email']
 		if commit:
 			user.save()
-<<<<<<< HEAD
-		return user
-=======
+
 		return user
 
-<<<<<<< HEAD
+		return user
 
-=======
-class UploadEvent(ModlelFrom):
-	
->>>>>>> f726452b778ad5e6e15ee4fe5a4249665ad5befb
->>>>>>> 80d43841a9ef19586ae3d60dd91f6ed00326450a
+
+#class UploadEvent(ModlelFrom):
+
--- peamine/kalender/models.py
+++ peamine/kalender/models.py
@@ -2,22 +2,20 @@ from django.db import models
 
 class Event(models.Model):
     Sündmus= models.CharField(max_length=100)
-<<<<<<< HEAD
+
     Olulisus= models.IntegerField(
         validators=[MaxValueValidator(5), MinValueValidator(1)]
     )
     Kuupäev= models.DateField(_(""), auto_now=False, auto_now_add=False)
-=======
+
     Lisa = models.CharField(max_length=150)
-<<<<<<< HEAD
+
     Olulisus= models.IntegerField()
     Alguskuupäev= models.DateField()
     Lõppkuupäev= models.DateField()
-=======
+
     Olulisus= models.IntegerField(
         validators=[MaxValueValidator(5), MinValueValidator(1)]
     )
-    Algus kuupäev= models.DateField()
-    Lõpp kuupäev= models.DateField()
->>>>>>> f726452b778ad5e6e15ee4fe5a4249665ad5befb
->>>>>>> 80d43841a9ef19586ae3d60dd91f6ed00326450a
+
+
