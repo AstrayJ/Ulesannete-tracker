f63dc3031fd18383413798c17992c81ed2cc14ed
ProgrammerBro1234 <karlerik.lehtma@real.edu.ee>
2023-05-24 10:40:12 +0300

    f

--- peamine/kalender/urls.py
+++ peamine/kalender/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'cal'
+app_name = 'kal'
 urlpatterns = [path('', views.index, name='index'),
 path('', views.CalendarView.as_view, name='kalender'),
 ]
