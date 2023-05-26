9a5b94e867aa014a05436316418a08565aa8be46
funny man <frankplays678@gmail.com>
2023-05-26 10:05:53 +0300

    mhm

--- peamine/kalender/urls.py
+++ peamine/kalender/urls.py
@@ -1,7 +1,4 @@
-
-<<<<<<< HEAD
 "https://www.huiwenteo.com/normal/2018/07/24/django-calendar.html"
-=======
 from django.urls import path
 from . import views
 
@@ -9,4 +6,3 @@ app_name = 'kal'
 urlpatterns = [path('', views.index, name='index'),
 path('', views.CalendarView.as_view, name='kalender'),
 ]
->>>>>>> 4da0716dd5de596fdb1fe83908718fb407b688d7
