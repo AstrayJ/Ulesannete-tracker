a99ac8ad139b110b795b1eea85c6584bee8eac82
AstrayJ <jarl.justus.hellat@gmail.com>
2023-06-02 09:50:35 +0300

    J

--- peamine/kalender/urls.py
+++ peamine/kalender/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('admin', admin.site.urls),
     path('', views.index, name='index'),
     path('kalender/', views.CalendarView.as_view(), name='kalender'),
-    path('Uus/', views.event, name='Uus'),
-	path('Muutus', views.event, name='event_edit'),
+    path('kalender/Uus', views.event, name='Uus'),
+	path('kalender/event_edit', views.event, name='event_edit'),
 ]
