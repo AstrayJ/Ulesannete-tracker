988b73f2c86ee3b46f71c62ff59331adbc636c8a
AstrayJ <131436862+AstrayJ@users.noreply.github.com>
2023-06-08 17:34:50 +0300

    Update urls.py

--- peamine/kalender/urls.py
+++ peamine/kalender/urls.py
@@ -7,11 +7,9 @@ urlpatterns = [
     path('admin', admin.site.urls),
     path('', views.index, name='index'),
     path('kalender/', views.CalendarView.as_view(), name='kalender'),
-<<<<<<< HEAD
     path('Uus/', views.event, name='Uus'),
 	path('Muutus/', views.event, name='event_edit'),
-=======
     path('kalender/Uus', views.event, name='Uus'),
 	path('kalender/event_edit', views.event, name='event_edit'),
->>>>>>> 6ffb3bd3c5509a816a50f4f785407cf6a7ecaafd
+
 ]
